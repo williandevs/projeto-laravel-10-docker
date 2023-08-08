bcd3fc42cca795c55fc460d7d16866c3c5329c17
williandevs <williamdev38@gmail.com>
2023-08-08 17:04:29 -0300

    fron-end

---
+++ resources/views/components/navegacao.blade.php
@@ -0,0 +1,32 @@
+<div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
+  <nav class="nav flex-column">
+      <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
+          <svg class="bi">
+              <use xlink:href="#house-fill" />
+          </svg>
+          Painel
+      </a>
+      <a class="nav-link d-flex align-items-center gap-2" href="#">
+          <svg class="bi">
+              <use xlink:href="#file-earmark" />
+          </svg>
+          Venda
+      </a>
+      <a class="nav-link d-flex align-items-center gap-2" href="#">
+          <svg class="bi">
+              <use xlink:href="#cart" />
+          </svg>
+          Produtos
+      </a>
+      <a class="nav-link d-flex align-items-center gap-2" href="#">
+          <svg class="bi">
+              <use xlink:href="#people" />
+          </svg>
+          Clientes
+      </a>
+      
+     
+  </nav>
+
+
+</div>
