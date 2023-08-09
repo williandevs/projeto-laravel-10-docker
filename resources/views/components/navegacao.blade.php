32162fec3185a65e2e0c72021bf512e26c81d2f7
williandevs <williamdev38@gmail.com>
2023-08-09 15:28:17 -0300

    referenciar rota no front

--- resources/views/components/navegacao.blade.php
+++ resources/views/components/navegacao.blade.php
@@ -12,7 +12,7 @@
           </svg>
           Venda
       </a>
-      <a class="nav-link d-flex align-items-center gap-2" href="#">
+      <a class="nav-link d-flex align-items-center gap-2" href="{{ route('produto.index') }}">
           <svg class="bi">
               <use xlink:href="#cart" />
           </svg>
